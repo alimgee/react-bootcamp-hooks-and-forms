70da872f3624f0165bc140845cc39fd7e73d48cd
Alan Mc Gee <mcgee.al@gmail.com>
2021-06-02 13:43:23 +0100

    added materialui imports to ToDoApp

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,5 +1,13 @@
+import react from 'react';
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+
+
 function ToDoApp(){
-    return <h1>To Dos</h1>
+    return <h1>To Dos</h1>;
 }
 
-export default ToDoApp
+export default ToDoApp;
