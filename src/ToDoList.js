f2c3e3003e9a2f77b7eb93bbde523e51cbd44db2
Alan Mc Gee <mcgee.al@gmail.com>
2021-06-03 12:16:29 +0100

    fixing initial list item

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItem';
+import Divider from "@material-ui/core/Divider";
 
-function ToDoList(props){
-    console.log({props});
-    return(
-        
-        <div>
-            
-            {
-            props.todos.map(todo =>(
-                
-                <li>{todo.task}</li>
-                
-            ))}
-        </div>
+function ToDoList(props) {
+    console.log({ props });
+    return (
+        <Paper>
+            <List>
+                {props.todos.map(todo => (
+                    <>
+                        <ListItem>
+                            <ListItemText>{todo.task}</ListItemText>
+                        </ListItem>
+                        <Divider />
+                    </>
+
+                ))}
+            </List>
+        </Paper>
+
     )
 }
 
